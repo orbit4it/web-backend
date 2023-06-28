f87b796181a16b10ba9b266eecb038e875df3578
Diaz-Adrianz <diazadrianz191205@gmail.com>
2023-06-29 02:15:21 +0700

    Lupa beberapa baris kode

--- script.py
+++ script.py
@@ -3,19 +3,17 @@ import sys
 if len(sys.argv) < 2:
     exit()
 
+
 def seed_database():
-    from src.db import Session, tables
-    from src.core.user.model import User, UserPending, Role
-    from src.core.grade.model import Grade, GradeLevel, Vocational
     from src.core.division.model import Division
+    from src.core.grade.model import Grade
+    from src.core.grade.type import GradeLevel, Vocational
+    from src.core.user.model import Role, User, UserPending
+    from src.db import Session, tables
 
     db = Session()
 
-    grade = Grade(
-        grade=GradeLevel.XII,
-        vocational=Vocational.TKI,
-        name="XII PPLG 1"
-    )
+    grade = Grade(grade=GradeLevel.XII, vocational=Vocational.TKI, name="XII PPLG 1")
 
     division = Division(
         name="Game Development",
@@ -26,21 +24,19 @@ def seed_database():
         name="John Doe",
         email="johndoe@gmail.com",
         motivation="Here is my motivation",
-
         division_id=1,
         grade_id=1,
     )
 
     user = User(
-        name = "John Doe",
-        email = "johndoe@gmail.com",
-        password = "password",
-        role = Role.user,
-        refresh_token = "123456789",
-        phone_number = "08123456789",
-
-        division_id = 1,
-        grade_id = 1,
+        name="John Doe",
+        email="johndoe@gmail.com",
+        password="password",
+        role=Role.user,
+        refresh_token="123456789",
+        phone_number="08123456789",
+        division_id=1,
+        grade_id=1,
     )
 
     db.add(grade)
@@ -49,11 +45,11 @@ def seed_database():
     db.add(user)
     db.commit()
 
-if __name__ == '__main__':
 
+if __name__ == "__main__":
     if sys.argv[1] == "seed":
         seed_database()
     elif sys.argv[1] == "drop-all":
         from src.db import tables
-        tables.drop_all()
 
+        tables.drop_all()
