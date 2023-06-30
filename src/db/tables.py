c5a84442e98200cbcf886e3779b4c50067969272
Diaz-Adrianz <diazadrianz191205@gmail.com>
2023-06-30 14:12:58 +0700

    ketinggalan import model baru

--- src/db/tables.py
+++ src/db/tables.py
@@ -1,10 +1,12 @@
-import src.core.user.model
-import src.core.grade.model
 import src.core.division.model
+import src.core.grade.model
+import src.core.schedule.model
+import src.core.user.model
 
 from .session import Base, engine
 
 Base.metadata.create_all(engine)
+
 
 def drop_all():
     Base.metadata.drop_all(engine)
