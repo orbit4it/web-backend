b6ff6d660e57912cf2da9a3e31824b5a6fb5127b
najwanaribena <semuatentanggadget@gmail.com>
2023-07-08 07:41:55 +0700

    fix: close database session dependency

--- src/schema/__init__.py
+++ src/schema/__init__.py
@@ -1,15 +1,18 @@
 from fastapi import Depends
 from strawberry import Schema
 from strawberry.fastapi import GraphQLRouter
-from sqlalchemy.orm import Session as _Session
 
 from db.session import Session
 from .query import Query
 from .mutation import Mutation
 
 
-def get_db_session() -> _Session:
-    return Session()
+async def get_db_session():
+    session = Session()
+    try:
+        yield session
+    finally:
+        session.close
 
 
 async def get_context(db=Depends(get_db_session)):
